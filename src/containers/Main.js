c898c740e127fa044421bec3170f6ae1f0e2b7aa
Enmanuel Almonte <ealmonterozon@gmail.com>
2017-09-16 09:17:44 -0400

    Fixing main container indentation

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,24 +3,24 @@ import Main from '../components/Main/Main';
 import { getResource } from '../utils/Request';
 
 class MainContainer extends Component {
-    state = {
-        gifs: []
-    };
-    componentDidMount () {
-        getResource('hello').then((response) => {
-                this.setState((prevState, props) => {
-                    return {gifs: response}
-                });
-            }
-        ).catch(error => console.error(error));
-    };
-    render () {
-        return (
-            <Main
-                gifs={this.state.gifs}
-            />
-        );
-    }
+  state = {
+    gifs: []
+  };
+  componentDidMount () {
+    getResource('hello').then((response) => {
+      this.setState((prevState, props) => {
+        return {gifs: response}
+        });
+      }
+    ).catch(error => console.error(error));
+  };
+  render () {
+    return (
+      <Main
+        gifs={this.state.gifs}
+      />
+    );
+  }
 }
 
 export default MainContainer;
